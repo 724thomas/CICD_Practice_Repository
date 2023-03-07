23870eb25c6adb84bff85a83f3c5e1b27ea375e6
ThomasChoi <724thomas@gmail.com>
2023-03-07 15:00:17 +0900

    Initial commit

---
+++ src/main/java/com/example/basicjavaproject/BasicJavaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.basicjavaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicJavaProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicJavaProjectApplication.class, args);
+    }
+
+}
